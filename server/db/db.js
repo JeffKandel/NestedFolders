a9b187d9c17f80011f1833d60f4f8797459d5cdd
Jeff Kandel <jeff.kandel@gmail.com>
2018-04-27 12:23:52 -0400

    Update db.js to work while deployed

--- server/db/db.js
+++ server/db/db.js
@@ -7,8 +7,11 @@ const databaseName = pkg.name + (process.env.NODE_ENV === 'test' ? '-test' : '')
  * To provide credentials for postgres use:
  * postgres://USERNAME:PASSWORD@localhost:5432/${databaseName}
  */
-const db = new Sequelize(`postgres://localhost:5432/${databaseName}`, {
-  logging: false
-})
+const db = new Sequelize(
+  process.env.DATABASE_URL || `postgres://localhost:5432/${databaseName}`,
+  {
+    logging: false
+  }
+)
 
 module.exports = db
