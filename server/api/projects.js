05f107d4e172b1edcd4118e780bec6d99b453354
Jeff Kandel <jeff.kandel@gmail.com>
2018-04-27 09:41:36 -0400

    Query for project with nested root folder

--- server/api/projects.js
+++ server/api/projects.js
@@ -1,6 +1,19 @@
 const router = require('express').Router()
+const { Project, Folder } = require('../db/models')
+
 module.exports = router
 
 router.get('/:id', (req, res, next) => {
-  res.json(req.params.id)
+  const { id } = req.params
+
+  Project.findById(id, {
+    include: [
+      {
+        model: Folder,
+        as: 'root_folder'
+      }
+    ]
+  })
+  .then(project => res.json(project))
+  .catch(next)
 })
--- server/db/models/index.js
+++ server/db/models/index.js
@@ -2,6 +2,9 @@ const File = require('./file')
 const Folder = require('./folder')
 const Project = require('./project')
 
+/**
+ * Associations establish foreign keys and allow for eager loading of models
+ */
 File.belongsTo(Folder, {foreignKey: 'folder_id'})
 Folder.belongsTo(Folder, {foreignKey: 'parent_folder_id'})
 Project.belongsTo(Folder, {
