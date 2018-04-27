9be7ce3bbdff19a863276592a48bd0e692c972e4
Jeff Kandel <jeff.kandel@gmail.com>
2018-04-27 11:38:54 -0400

    Add comments to app.js

--- server/app.js
+++ server/app.js
@@ -5,12 +5,25 @@ const db = require('./db')
 const app = express()
 module.exports = app
 
+/**
+ * The app is composed of three functions: createApp, startListening, syncDb
+ * This makes the logic between starting the app and testing it easy to handle (lines 39-50)
+ */
+
 const createApp = () => {
+  // logging middleware
   app.use(morgan('dev'))
+
+  // api routes
   app.use('/api', require('./api'))
+
+  // send a 404 for other requests
   app.use('*', (req, res, next) => {
-    res.send('Consider exploring <a href="/api/projects/1">project 1</a> or <a href="/api/projects/2">project 2</a>')
+    res.status(404)
+    res.send('Not Found... consider exploring <a href="/api/projects/1">project 1</a> or <a href="/api/projects/2">project 2</a>')
   })
+
+  // error handling endware
   app.use((err, req, res, next) => {
     console.error(err)
     console.error(err.stack)
@@ -22,6 +35,12 @@ const startListening = () => app.listen(PORT, () => console.log(`Check me out on
 
 const syncDb = () => db.sync()
 
+
+/*
+ * `require.main === module`:
+ * Evaluates to true when run from the command line with `node server/app.js`
+ * Evaluates to false when required by the testing module
+ */
 if (require.main === module) {
   syncDb()
   .then(createApp)
